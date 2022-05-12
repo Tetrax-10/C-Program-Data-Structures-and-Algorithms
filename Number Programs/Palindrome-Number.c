06235b61efebd69d1d9d9cebe828d3a40a2a3316
Raghavan <raghavanmarch2002@gmail.com>
2022-05-12 17:07:51 +0530

    added note

--- Number Programs/Palindrome-Number.c
+++ Number Programs/Palindrome-Number.c
@@ -10,11 +10,15 @@ int main()
     int num, dig, res = 0, pv = 1;
     scanf("%d", &num);
     while (num / pv)
-    {
+    {                                      // this method is for not destroying the number
         dig = (num / pv) % 10;
         res = res * 10 + dig;
         pv = pv * 10;
     }
+    // while (num) {                       // Easy but destroys number
+    //     res = res * 10 + (num % 10);
+    //     num /= 10;
+    // }
     if (num == res)
         printf("Palindrome");
     else
