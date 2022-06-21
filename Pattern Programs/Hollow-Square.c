458fc5bcc954b5d5ac1ae3a6c2ae32f041078d06
Paul Gates <paulmichaelgates@gmail.com>
2022-06-21 15:50:54 -0700

    Implemented Hollow-Square.c solution

--- Pattern Programs/Hollow-Square.c
+++ Pattern Programs/Hollow-Square.c
@@ -1,7 +1,46 @@
-// Hollow Square
+/*
+ Hollow Square
+
+ given a side length create a hollow square of LxL
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+ 
+// helper func
+void println(int len);
 
 // Solution :
+int main(){
+    
+    int len;
 
+    printf("Please enter the side length of the square\n");
+    scanf("%d", &len);
+    
+    char gap[len];
 
+    memset(gap, ' ', len);
+    gap[len] = '\0';
 
-// Code will be Updated Soon
+    println(len);
+    
+    for(int i = 0; i < len; i++)
+        printf("*%s*\n", gap);
+                 
+    println(len);
+
+    return 0;
+}
+
+// helper func
+void println(int len){
+    printf(" ");
+    for(int i = 0; i < len; i++)
+        printf("*");
+        
+    printf("\n");
+
+}
+ 
